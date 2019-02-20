304a3c46b3e5a10a583c70ea1d7f9c36e626e144
YUTA <raspberrypi7777777@gmail.com>
2019-02-20 14:31:44 +0900

    Enabled PWA

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = 'tetris_caches';
+var appShellFiles = [
+  '/Tetris/',
+  '/Tetris/index.html',
+  '/Tetris/images/app.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(appShellFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName &&  cacheName.startsWith("tetris-caches")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
